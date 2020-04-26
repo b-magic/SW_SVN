d44e65572080ac7528e05d6c28f1fec90ccdb782
b-magic <iben@live.ca>
2020-04-25 23:33:03 -0400

    done all subs but not checked

--- SolidWorksSVN/SwAddin.vb
+++ SolidWorksSVN/SwAddin.vb
@@ -523,8 +523,27 @@ Public Class SwAddin
             PMPEnable = 1
         End If
     End Function
+    Sub myUnlockActive()
+        unlockDocs(iSwApp.ActiveDoc())
+    End Sub
+    Sub myUnlockActiveWithDependents()
+        unlockDocs(getComponentsOfAssembly(iSwApp.ActiveDoc()))
+    End Sub
+    Sub myUnlockAll()
+        Dim bSuccess As Boolean = runTortoiseProcexeWithMonitor("/command:unlock /path:" & sRepoLocalPath & " /closeonend:3")
+        If Not bSuccess Then iSwApp.SendMsgToUser("Releasing Locks Failed.")
+    End Sub
 
-    Sub myCheckinActiveDoc()
+    Sub unlockDocs(ByRef modDocArr() As ModelDoc2)
+        Dim bSuccess As Boolean = runTortoiseProcexeWithMonitor("/command:unlock /path:" &
+                                         formatFilePathArrForTortoiseProc(
+                                            getFilePathsFromModDocArr(modDocArr)) & " /closeonend:3")
+
+        If Not bSuccess Then iSwApp.SendMsgToUser("Releasing Locks Failed.")
+
+    End Sub
+
+    Sub myCheckinActiveDocWithDependents()
         Dim modDocArr1() As ModelDoc2 = {iSwApp.ActiveDoc()}
         If modDocArr1(0).GetType = swDocumentTypes_e.swDocASSEMBLY Then
             checkInDocs(getComponentsOfAssembly(modDocArr1(0)))
@@ -593,22 +612,40 @@ Public Class SwAddin
 
         Dim sOpenDocPath() As String = getFilePathsFromModDocArr(OpenDocModels)
 
-        'Run SVN to see status of all
-        Dim mySVNStatus As SVNStatus() = getFileSVNStatus(sOpenDocPath, bCheckServer:=False)
-
-        'If modDoc.modDoc.GetPathName is in list of all local items in vault
-        ' AND does not have a correct key then set read-only
-
         setReadWriteFromLockStatus(OpenDocModels)
 
     End Sub
 
     Sub myCheckoutActiveDoc()
+        checkoutDocs({iSwApp.ActiveDoc()})
+    End Sub
+    Sub myCheckoutWithDependents()
+        checkoutDocs(getComponentsOfAssembly(iSwApp.ActiveDoc()))
+    End Sub
+    Sub myRepoStatus()
+        Dim bSuccess As Boolean
+        bSuccess = runTortoiseProcexeWithMonitor("/command:repostatus /path:" & sRepoLocalPath & " /remote")
+        If Not bSuccess Then iSwApp.SendMsgToUser("Status Check Failed.")
+    End Sub
+    Sub myCleanupAndRelease()
+        Dim bSuccess As Boolean
+        setReadWriteFromLockStatus(getAllOpenDocs(bMustBeVisible:=False))
+
+        bSuccess = runTortoiseProcexeWithMonitor("/command:cleanup /cleanup /path:" & sRepoLocalPath)
+        If Not bSuccess Then iSwApp.SendMsgToUser("Cleanup Failed. This is often because the SVN server is attempting " &
+                       "to open a file that SolidWorks is currently accessing. This occurs even when the file is read only. " &
+                       "Try closing all open files and trying again. Or close SolidWorks and use ToroiseSVN to clean up. ")
+    End Sub
+
+    Sub checkoutDocs(ByRef modDocArr() As ModelDoc2)
         Dim modDoc As ModelDoc2 = iSwApp.ActiveDoc()
-        Dim modDocArr() As ModelDoc2 = {modDoc}
-        Dim sActiveDocPath() As String = {modDoc.GetPathName()}
-        Dim status As SVNStatus
+        'Dim modDocArr() As ModelDoc2 = {modDoc}
+        Dim sActiveDocPath() As String = getFilePathsFromModDocArr(modDocArr)
+        Dim sDocPathsToCheckout(modDocArr.Length - 1) As String
+        Dim status() As SVNStatus
         Dim bSuccess As Boolean = False
+        Dim i As Integer
+        Dim sCatMessage As String = ""
 
         'Using objReader As System.IO.TextReader = System.IO.File.OpenText("C:\Users\benne\AppData\Local\Temp\tmp9BF6.tmp") 'System.IO.StreamReader(sTempFileName)
         '    sLine1 = objReader.ReadLine()
@@ -617,44 +654,60 @@ Public Class SwAddin
         ''objReader.Close()
         ''My.Computer.FileSystem.DeleteFile(sTempFileName)
         'Debug.Print(sTempFileName)
-        status = getFileSVNStatus(sActiveDocPath, True)(0)
+        status = getFileSVNStatus(sActiveDocPath, True)
 
-        If status.errorMessage <> "" Then
+        If status(0).errorMessage(0) <> "" Then
             ' Some sort of error from the svn command
-            If status.errorMessage.Contains("W155007:") Then
-                'Common error. File not saved into repository
-                iSwApp.SendMsgToUser("Status Check failed." & vbCrLf &
-                        "Error: File is not saved inside repository. " &
-                        "Save the file inside the repository and try again" &
-                        vbCrLf & "Error message:" & vbCrLf & status.errorMessage)
-            ElseIf status.errorMessage = "Status Returned from SVN Server with No Items" Then
-                iSwApp.SendMsgToUser("Current save file For model Not found. Save it inside the repository.")
-                Exit Sub
-            Else
-                'Other Errors
-                iSwApp.SendMsgToUser("Status Check failed." &
-                        vbCrLf & "Error message:" & vbCrLf & status.errorMessage)
-            End If
-            Exit Sub
+            For i = 0 To status(0).errorMessage.Length - 1
+                If status(0).errorMessage(i).Contains("W155007:") Then
+                    'Common error. File not saved into repository
+                    sCatMessage &= vbCrLf &
+                        status(0).errorFile(i) & vbCrLf &
+                            "Error W155007 File is not saved inside repository. " &
+                            "Save the file inside the repository and try again. "
+
+                ElseIf status(0).errorMessage(i) = "Status Returned from SVN Server with No Items" Then
+                    sCatMessage &= vbCrLf &
+                        status(0).errorFile(i) & vbCrLf &
+                    "Error: Current save file For model Not found. Save it inside the repository."
+                Else
+                    'Other Errors
+                    sCatMessage &= vbCrLf &
+                        status(0).errorFile(i) & vbCrLf &
+                            "Error: " & status(0).errorMessage(i)
+                End If
+            Next
+            iSwApp.SendMsgToUser("Status Check failed with the following " & sCatMessage)
+            If status(0).upToDate9 Is Nothing Then Exit Sub
         End If
+
+        If status(0).upToDate9 Is Nothing Then iSwApp.SendMsgToUser("Error: No Files found")
+
+        sCatMessage = ""
         ' File was found on vault!
-        If status.upToDate9 = "*" Then
-            'Vault has a newer version
-            iSwApp.SendMsgToUser("Local copy is out of date. Update from Vault and try again.")
-            Exit Sub
+        For i = 0 To status.Length - 1
+            If status(i).upToDate9 Is Nothing Then Continue For
+            If status(i).upToDate9 = "*" Then
+                'Vault has a newer version
+                sCatMessage &= vbCrLf & status(i).filename
+
+            ElseIf status(i).lock6 = "O" Then
+                ' File locked for editing by another user!
+                ' We'll just call the lock TortoiseProc process and let it+user deal with it
+                'iSwApp.SendMsgToUser("File is locked by another user. Ask them to check it back in. " &
+                '     "If you steal their lock (Not recommended) then " &
+                '     vbCrLf & "1. Make sure you discuss it with them so that work isn't lost!" &
+                '     vbCrLf & "2. In SolidWorks you'll have to do file > Get Write Access after you have the lock.")
+            Else
+                sDocPathsToCheckout(i) = status(i).filename
+            End If
+        Next
+
+        If sCatMessage <> "" Then
+            iSwApp.SendMsgToUser("Local copy is out of date. Update from Vault and try again." & vbCrLf & sCatMessage)
         End If
-        If status.lock6 = "O" Then
-            ' File locked for editing by another user!
-            ' We'll just call the lock TortoiseProc process and let it+user deal with it
-            iSwApp.SendMsgToUser("File is locked by another user. Ask them to check it back in. " &
-                     "If you steal their lock (Not recommended) then " &
-                     vbCrLf & "1. Make sure you discuss it with them so that work isn't lost!" &
-                     vbCrLf & "2. In SolidWorks you'll have to do file > Get Write Access after you have the lock.")
-        End If
-        bSuccess = runTortoiseProcexeWithMonitor("/command:lock /path:""" & sActiveDocPath(0) & """ /closeonend:3")
-        If Not bSuccess Then
-            Exit Sub
-        End If
+
+        bSuccess = runTortoiseProcexeWithMonitor("/command:lock /path:" & formatFilePathArrForTortoiseProc(sDocPathsToCheckout) & " /closeonend:3")
 
         setReadWriteFromLockStatus(modDocArr)
 
@@ -874,6 +927,7 @@ Public Class SwAddin
         Dim sStolenLockPaths As String = ""
         Dim i As Integer
         Dim j As Integer
+        Dim sCatErrorMessage As String = ""
 
         ' Now the file couldve been locked or not by the user in the dialog box
         ' So we'll query the vault and see what happened
@@ -884,9 +938,13 @@ Public Class SwAddin
         Next
 
         status = getFileSVNStatus(sDocPaths, False)
-        If status(0).errorMessage <> "" Then
-            iSwApp.SendMsgToUser("Status Check failed." &
-                        vbCrLf & "Error message:" & vbCrLf & status(0).errorMessage)
+        If status(0).errorMessage(0) <> "" Then
+            For i = 0 To status(0).errorMessage.Length - 1
+                sCatErrorMessage &= vbCrLf & status(0).errorFile(i) &
+                    vbCrLf & status(0).errorMessage(i)
+            Next
+            iSwApp.SendMsgToUser("Status Check Issues: " &
+                        vbCrLf & sCatErrorMessage)
         End If
 
         For i = 0 To sDocPaths.Length - 1
@@ -925,6 +983,8 @@ Public Class SwAddin
         Dim iLineStep As Integer = 1
         Dim i As Integer = 0
         Dim j As Integer = 0
+        Dim k As Integer = 0
+        Dim bExpectStatusAgainstRevision As Boolean = False
 
         'TODO: Error check. One file from list is not in server folder.
 
@@ -951,66 +1011,74 @@ Public Class SwAddin
         End Using
         sOutputLines = SVNOutput.Split(ControlChars.CrLf.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
         sOutputErrorLines = SVNErrorOutput.Split(ControlChars.CrLf.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-
-        For i = 0 To sOutputErrorLines.Length - 1
-            df
-
-        Next
-
+        k = sOutputErrorLines.Length - 1
 
         'Error Checking
-        If output(0).errorMessage Is Nothing Then
-            output(0).errorMessage = "Error: SVN status output standard error is nothing. Must of not connected/read to SVN process"
-            Return output
-        ElseIf output(0).errorMessage <> "" Then
-            Return output
-        ElseIf sOutputLines(0) Is Nothing Then
-            output(0).errorMessage = "Error: Could not read SVN Status. Returned Empty Line"
+        If (sOutputErrorLines(0) Is Nothing) Or (sOutputLines(0) Is Nothing) Then
+            output(0).errorMessage(i) = "Error: SVN status output standard error is nothing. Must of not connected/read to SVN process"
             Return output
         End If
 
+        For i = 0 To sOutputErrorLines.Length - 1
+            'All error messages are an array stored in the first element of the output structure
+            output(0).errorMessage(i) = sOutputErrorLines(i)
+        Next
+
         If (bCheckServer) Then
             If sOutputLines(0).Substring(0, 23) = "Status against revision" Then
-                output(0).errorMessage = "Status Returned from SVN Server with No Items" 'Don't edit this without also changing if statements that check if errormessage is this message 
+                output(0).errorMessage(0) = "Status Returned from SVN Server with No Items" 'If you change the string, change it other places in the code too!
                 Return output
 
             ElseIf (sOutputLines.Length = 1) Then
                 'If we are checking the server, we should expect a line 2. If its not there then theres an error.
-                output(0).errorMessage = "Error: Incomplete SVN Status. Could not Read Line 2. Line 1:" & sOutputLines(0)
+                output(0).errorMessage(0) = "Error: Incomplete SVN Status. Could not Read Line 2. Line 1:" & sOutputLines(0)
                 Return output
                 'ElseIf (sOutputLines(0).Contains(sFilePath)) And (sOutputLines(1).Substring(0, 23) = "Status against revision") Then
             Else
-                bSuccess = True
-                iLineStep = 2 ' skips every second line
+                'bSuccess = True
+                '               iLineStep = 2 ' skips every second line
             End If
         Else 'ElseIf (sOutputLines(0).Contains(sFilePath)) Then
-            bSuccess = True
-            iLineStep = 1
+            'bSuccess = True
+            'iLineStep = 1
         End If
-        If bSuccess Then
-            ' Success
-            'ReDim Preserve output((sOutputLines.Length - 1) / (iLineStep + 1)) 'server tags an extra line on. True = -1, so this stops before the extra line
+        'If bSuccess Then
+        ' Success
+        'ReDim Preserve output((sOutputLines.Length - 1) / (iLineStep + 1)) 'server tags an extra line on. True = -1, so this stops before the extra line
 
-            For i = 0 To sOutputLines.Length - 1 Step iLineStep
-                output(j).filename = sOutputLines(i).Substring(41, sOutputLines(i).Length - 41)
-                output(j).addDelChg1 = sOutputLines(i).Substring(0, 1)
-                output(j).modifications2 = sOutputLines(i).Substring(1, 1)
-                output(j).workingDirLock3 = sOutputLines(i).Substring(2, 1)
-                output(j).addWithHist4 = sOutputLines(i).Substring(3, 1)
-                output(j).switchWParent5 = sOutputLines(i).Substring(4, 1)
-                output(j).lock6 = sOutputLines(i).Substring(5, 1)
-                output(j).tree7 = sOutputLines(i).Substring(6, 1)
-                'col 8 is blank
-                If bCheckServer Then
-                    output(j).upToDate9 = sOutputLines(i).Substring(8, 1)
+        For i = 0 To sOutputLines.Length - 1 'Step iLineStep
+            If sOutputLines(i).Substring(0, 23) = "Status against revision" Then
+                If bExpectStatusAgainstRevision Then
+                    bExpectStatusAgainstRevision = False
+                    Continue For
                 Else
-                    output(j).upToDate9 = "NoUpdate"
+                    output(0).errorMessage(k) = "Status Returned from SVN Server with No Items"
+                    output(0).errorFile(k) = sOutputLines(i).Substring(41, sOutputLines(i).Length - 41)
+                    bExpectStatusAgainstRevision = False
+                    Continue For
                 End If
-                j = j + 1
-            Next i
-        Else
-            output(0).errorMessage = "Error. First read line from SVN Status: " & sOutputLines(0)
-        End If
+            End If
+
+            output(j).filename = sOutputLines(i).Substring(41, sOutputLines(i).Length - 41)
+            output(j).addDelChg1 = sOutputLines(i).Substring(0, 1)
+            output(j).modifications2 = sOutputLines(i).Substring(1, 1)
+            output(j).workingDirLock3 = sOutputLines(i).Substring(2, 1)
+            output(j).addWithHist4 = sOutputLines(i).Substring(3, 1)
+            output(j).switchWParent5 = sOutputLines(i).Substring(4, 1)
+            output(j).lock6 = sOutputLines(i).Substring(5, 1)
+            output(j).tree7 = sOutputLines(i).Substring(6, 1)
+            'col 8 is blank
+            If bCheckServer Then
+                output(j).upToDate9 = sOutputLines(i).Substring(8, 1)
+            Else
+                output(j).upToDate9 = "NoUpdate"
+            End If
+            j = j + 1
+            If bCheckServer Then bExpectStatusAgainstRevision = True
+        Next i
+        'Else
+        '   output(0).errorMessage = "Error. First read line from SVN Status: " & sOutputLines(0)
+        'End If
         Return output
     End Function
     Function getComponentsOfAssembly(ByRef modDoc As ModelDoc2) As ModelDoc2()
@@ -1053,7 +1121,8 @@ Public Class SwAddin
         Public tree7 As String
         'col 8 is blank
         Public upToDate9 As String
-        Public errorMessage As String
+        Public errorMessage() As String
+        Public errorFile() As String
     End Structure
 
 #End Region
